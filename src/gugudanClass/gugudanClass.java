cb09c3693af19e49918c409e7c3b1f270eef7b7c
freerole <freerole@218.38.137.28>
2021-05-16 12:32:13 +0900

    gugudan.Class

--- src/gugudanClass/gugudanClass.java
+++ src/gugudanClass/gugudanClass.java
@@ -1,5 +1,17 @@
 package gugudanClass;
 
 public class gugudanClass {
-
+	public static int[] calculate(int item){
+		int[] result = new int[9];
+		for(int i =0; i<10; i++) {
+			result[i] = item * (i + 1);
+		}
+		return result;
+	}
+	public static void print(int[] result) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
 }
--- src/gugudanClass/gugudanClassMain.java
+++ src/gugudanClass/gugudanClassMain.java
@@ -1,5 +1,10 @@
 package gugudanClass;
 
 public class gugudanClassMain {
-
+	public static void main(String args) {
+		for(int i = 0; i < 10; i++) {
+			int[] result = gugudanClass.calculate(i);
+			gugudanClass.print(result);
+		}
+	}
 }
