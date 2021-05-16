62000bd127d1188b415bf2151a6d92b3c7d1d646
freerole <freerole@218.38.137.28>
2021-05-16 22:29:32 +0900

    깔끔하게 출력이 되도록 수

--- src/gugudanClass/gugudanClass.java
+++ src/gugudanClass/gugudanClass.java
@@ -1,18 +1,27 @@
 package gugudanClass;
 
 public class gugudanClass {
-	public static int[] calculate(int item){
-		int[] result = new int[9];
-		for(int i = 0; i < 9; i++) {
+	public static int[] calculate(int item, int second){
+		int[] result = new int[second];
+		for(int i = 0; i < second; i++) {
 			result[i] = item * (i + 1);
 		}
 		return result;
 	}
 	
 	public static void print(int[] result) {
-		for(int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
+		int count = result.length;
+		int countComma = 1;
+		for(int i = 0; i < count; i++) {
+			System.out.print(result[i]);
+			if(countComma == count) {
+				System.out.print("");
+			}else {
+				System.out.print(", ");
+			}
+			countComma++;
 		}
+		System.out.println();
 	}
 	
 }
--- src/gugudanClass/gugudanClassMain.java
+++ src/gugudanClass/gugudanClassMain.java
@@ -1,9 +1,19 @@
 package gugudanClass;
+import java.util.Scanner;
 
 public class gugudanClassMain {
 	public static void main(String[] args) {
-		for(int i = 2; i < 10; i++) {
-			int[] result = gugudanClass.calculate(i);
+		Scanner scanf = new Scanner(System.in);
+		String inputValue = scanf.nextLine();
+		String[] splitedValue = inputValue.split(",");
+
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		System.out.println("1번 : "+ first + "  " + "2번 : " + second);
+		
+		for(int i = 2; i <= first; i++) {
+			int[] result = gugudanClass.calculate(i, second);
 			gugudanClass.print(result);
 		}
 	}
