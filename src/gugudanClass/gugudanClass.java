655ae8f0f2a42e8ebf762ad2c6f66fd3df1af59e
freerole <freerole@218.38.137.28>
2021-05-16 12:56:23 +0900

    gugudan.Class.수정

--- src/gugudanClass/gugudanClass.java
+++ src/gugudanClass/gugudanClass.java
@@ -3,13 +3,14 @@ package gugudanClass;
 public class gugudanClass {
 	public static int[] calculate(int item){
 		int[] result = new int[9];
-		for(int i =0; i<10; i++) {
+		for(int i = 0; i < 9; i++) {
 			result[i] = item * (i + 1);
 		}
 		return result;
 	}
+	
 	public static void print(int[] result) {
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
 	}
--- src/gugudanClass/gugudanClassMain.java
+++ src/gugudanClass/gugudanClassMain.java
@@ -1,8 +1,8 @@
 package gugudanClass;
 
 public class gugudanClassMain {
-	public static void main(String args) {
-		for(int i = 0; i < 10; i++) {
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
 			int[] result = gugudanClass.calculate(i);
 			gugudanClass.print(result);
 		}
